fe04ad146fd1dbb573d19c5a7e575d57a246ad1e
TeamC3-GP <TeamC3@GaryPedretti.com>
2024-12-05 17:45:05 +0000

    Added tests for position and parse position

--- Battleship_Tests/parsePositionTests.js
+++ Battleship_Tests/parsePositionTests.js
@@ -9,4 +9,35 @@ describe("parsePositionTests", function () {
     var actual = battleship.ParsePosition("B3");
     assert.deepStrictEqual(actual, expected);
   });
+
+  it("should return a valid position for valid input, lowercase", function () {
+    var expected = new position(letters.B, 3);
+    var actual = battleship.ParsePosition("b3");
+    assert.deepStrictEqual(actual, expected);
+  });
+
+  it("should return an expected string when toString is called", function () {
+    var expected = "B3";
+    var actual = battleship.ParsePosition("b3");
+    assert.deepStrictEqual(actual.toString(), expected);
+  });
+  
+  it("should return null for if letter is out of range", function () {
+    var expected = null;
+    var actual = battleship.ParsePosition("Z3");
+    assert.deepStrictEqual(actual, expected);
+  });
+  
+  it("should return null for if number is out of range", function () {
+    var expected = new position(letters.B, 3);
+    var actual = battleship.ParsePosition("B99");
+    assert.deepStrictEqual(actual, expected);
+  });
+  
+  it("should return null for if the 2nd and third characters are not a valid number", function () {
+    var expected = new position(letters.B, 3);
+    var actual = battleship.ParsePosition("Bzz");
+    assert.deepStrictEqual(actual, expected);
+  });
+  
 });
