5a2c488ac688e2378be1704b9c01c6a4bca6b0e0
TeamC4-GP <102955473+TeamC4-GP@users.noreply.github.com>
2025-04-23 17:22:34 +0000

    Add ship name

--- battleship.js
+++ battleship.js
@@ -25,7 +25,7 @@ class Battleship {
         console.log(cliColor.magenta("                    +---------------___[}-_===_.'____                 /\\"));
         console.log(cliColor.magenta("                ____`-' ||___-{]_| _[}-  |     |_[___\\==--            \\/   _"));
         console.log(cliColor.magenta(" __..._____--==/___]_|__|_____________________________[___\\==--____,------' .7"));
-        console.log(cliColor.magenta("|                        Welcome to Battleship                         BB-61/"));
+        console.log(cliColor.magenta("|                        Welcome to Battleship             USS IOWA     BB-61/"));
         console.log(cliColor.magenta(" \\_________________________________________________________________________|"));
         console.log();
 
