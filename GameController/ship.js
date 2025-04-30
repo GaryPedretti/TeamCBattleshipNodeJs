9f4c00b50138113822a19c8d501645927ad0eb30
TeamC1-GP <102695098+TeamC1-GP@users.noreply.github.com>
2025-04-30 13:52:04 +0000

    added hit counter

--- GameController/ship.js
+++ GameController/ship.js
@@ -4,11 +4,33 @@ class Ship {
         this.size = size;
         this.color = color;
         this.positions = [];
+        this.hits = size;
+        this.sunk = false;
+    
     }
-
     addPosition(position) {
         this.positions.push(position);
     }
+
+    getSunkMessage() {
+        return "Enemies " + this.name + " has been sunk";
+    }
+
+    getHitButNotSunkMessage() {
+        return "Enemies ship was hit but not sunk";
+    }
+
+    logHit() {
+        if(this.hits == 1 || this.hits == 0) {
+            this.sunk = true;
+            this.hits = 0;
+            return this.getSunkMessage();
+        }
+        this.hits--;
+        return this.getHitButNotSunkMessage();
+    }
+
+
 }
 
 module.exports = Ship;
